ee9469c4ae7a31745c582372ac6d7f8f710ace0f
D3nz13 <seba.szczepan2@gmail.com>
2021-07-18 16:54:07 +0200

    Made changes to the architecture of the cnn and the optimizer

--- cifar10.py
+++ cifar10.py
@@ -4,6 +4,7 @@ from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Dense, Flatten, BatchNormalization
+from tensorflow.keras.optimizers import SGD
 
 def plot_classes(X_train, y_train, num_classes, classes_names):
     """
@@ -42,28 +43,34 @@ def prepare_data(X_train, y_train, X_test, y_test, num_classes):
 def create_model():
     model = Sequential()
     model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu'))
+    model.add(BatchNormalization())
     model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
     model.add(BatchNormalization())
     model.add(MaxPool2D(pool_size=(2, 2), strides=2))
     model.add(Dropout(0.3))
 
     model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+    model.add(BatchNormalization())
     model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+    model.add(BatchNormalization())
     model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
     model.add(BatchNormalization())
     model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.4))
     model.add(Flatten())
 
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
+
+    optimizer = SGD(learning_rate=0.001, momentum=0.9)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
 
 def make_plots(history):
-    # plot accuracy
+    # plot the accuracy
     plt.figure(figsize=(8, 8))
     plt.subplot(2, 1, 1)
     plt.plot(history.history['accuracy'])
@@ -72,7 +79,7 @@ def make_plots(history):
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['train', 'test'])
-
+    # plot the loss
     plt.subplot(2, 1, 2)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -89,8 +96,8 @@ if __name__ == '__main__':
     # plot_classes(X_train, y_train, num_classes, classes_names)
     X_train, y_train, X_test, y_test = prepare_data(X_train, y_train, X_test, y_test, num_classes)
     cnn = create_model()
-    cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    history = cnn.fit(X_train, y_train, epochs=50, batch_size=32, verbose=1, validation_data=(X_test, y_test))
+    history = cnn.fit(X_train, y_train, epochs=50, batch_size=64, verbose=1, validation_data=(X_test, y_test))
     cnn.evaluate(X_test, y_test)
     make_plots(history)
-    cnn.summary()
+    # cnn.summary()
+    cnn.save('model.h5')
